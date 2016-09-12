52d92dabecd15de4f466a3185f20e16e436af2cf
Anthony Melouney <a.melouney@gmail.com>
2016-09-12 19:09:57 +1000

    added variables

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 console.log('Calculator');
+
+var clear = document.getElementById('clear').innerHTML;
+console.log(clear);
+var swap = document.getElementById('swap').innerHTML;
+console.log(swap);
+var percent = document.getElementById('percent').innerHTML;
+console.log(percent);
+var divide = document.getElementById('divide').innerHTML;
+console.log(divide);
+
+var _7 = document.getElementById('_7').innerHTML;
+console.log(_7);
+var _8 = document.getElementById('_8').innerHTML;
+console.log(_8);
+var _9 = document.getElementById('_9').innerHTML;
+console.log(_9);
+var multiply = document.getElementById('multiply').innerHTML;
+console.log(multiply);
+
+console.log();
+var _4 = document.getElementById('_4').innerHTML;
+console.log(_4);
+var _5 = document.getElementById('_5').innerHTML;
+console.log(_5);
+var _6 = document.getElementById('_6').innerHTML;
+console.log(_6);
+var subtract = document.getElementById('subtract').innerHTML;
+console.log(subtract);
+
+var _1 = document.getElementById('_1').innerHTML;
+console.log(_1);
+var _2 = document.getElementById('_2').innerHTML;
+console.log(_2);
+var _3 = document.getElementById('_3').innerHTML;
+console.log(_3);
+var add = document.getElementById('add').innerHTML;
+console.log(add);
+
+var _0 = document.getElementById('_0').innerHTML;
+console.log(_0);
+var decimal = document.getElementById('decimal').innerHTML;
+console.log(decimal);
+var equals = document.getElementById('equals').innerHTML;
+console.log(equals);
